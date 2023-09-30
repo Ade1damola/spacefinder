3bed0f7107b6c357e529b1bc3d1a97a2f9bdc77d
khidrhussein <husseinkhidr3@gmail.com>
2023-09-30 13:19:41 +0100

    message model added

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Space, Messages
 
+admin.site.register(Space)
+admin.site.register(Messages)
 
--- base/models.py
+++ base/models.py
@@ -2,3 +2,32 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Space(models.Model):
+    host = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=200)
+    school = models.CharField(max_length=200)
+    hostel = models.CharField(max_length=200)
+    room_number = models.CharField(max_length=5)
+    price = models.PositiveIntegerField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta():
+        ordering = ['-updated', '-created']
+    
+    def __str__(self):
+        return self.name
+    
+class Messages(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    space = models.ForeignKey(Space, on_delete=models.CASCADE, related_name="messages")
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta():
+        ordering = ['-updated', '-created']
+        
+    def __str__(self):
+        return self.body[0:50]
