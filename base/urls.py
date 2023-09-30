447521036c2eaef8bf78d36d25e0458f3abb104d
khidrhussein <husseinkhidr3@gmail.com>
2023-09-30 10:34:18 +0100

    initial commit

---
+++ base/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns =[
+    path('login', views.loginPage, name="login"),
+    path('logout', views.logoutUser, name="logout"),
+    path('register', views.registerPage, name="register"),
+
+    path('update-user/', views.updateUser, name="update-user"),
+
+    path('home', views.home, name="home"),
+    path('', views.landingPage, name="landing-page"),
+    path('space/', views.space, name="space"),
+    path('profile/<str:pk>/', views.userProfile, name="user-profile"),
+]
---
+++ base/views.py
@@ -0,0 +1,86 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm 
+from .forms import UserForm
+
+# Create your views here.
+def loginPage(request):
+    page = 'login'
+
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+
+        try:
+           user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password does not exist')
+
+
+    context = {'page': page}
+    return render(request, 'base/login_register.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
+
+def registerPage(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save() 
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occourred during registration!')
+
+    return render(request, 'base/login_register.html', {'form': form})
+
+
+def home(request):
+    return render( request, 'base/home.html')
+
+def landingPage(request):
+    return render( request, 'base/landing-page.html')
+
+def space(request):
+    return HttpResponse('AVAILABLE SPACES')
+
+
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    context = {'user':user}
+    return render(request, 'base/profile.html', context)
+
+
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+
+    return render(request, 'base/update-user.html', {'form': form})
