5b6e9da67c19fb559c233753abb18eab98c00b44
khidrhussein <husseinkhidr3@gmail.com>
2023-10-23 19:53:24 +0100

    unverified changes

--- base/urls.py
+++ base/urls.py
@@ -9,7 +9,7 @@ urlpatterns =[
     path('update-user/', views.updateUser, name="update-user"),
     path('profile/<str:pk>/', views.userProfile, name="user-profile"),
 
-    path('newsletter-subscription', views.subscribe_to_newsletter, name="subscribe_to_newsletter"),
+    path('newsletter_subscription', views.subscribe_to_newsletter, name="newsletter_subscription"),
 
     path('', views.home, name="home"),
     path('hostel/', views.hostel, name="hostel"),
